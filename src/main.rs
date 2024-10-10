f25103738af289d1291bcbb55428493020096f1b
Rain Caldwell <67359748+Just-a-Unity-Dev@users.noreply.github.com>
2024-10-10 17:38:08 +0800

    on-line input

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,32 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
+    let mut correct = 0;
+    let mut mistake = 0;
+
     let mut rng = rand::thread_rng();
  
     let upper_limit = 25;
     let lower_limit = 1;
 
-    let left_hand = rng.gen_range(lower_limit..=upper_limit);
-    let right_hand = rng.gen_range(lower_limit..=upper_limit);
-    let answer = left_hand + right_hand;
+    loop {
+        let left_hand = rng.gen_range(lower_limit..=upper_limit);
+        let right_hand = rng.gen_range(lower_limit..=upper_limit);
+        let answer = left_hand + right_hand;
 
-    print!("{} + {} = ", left_hand, right_hand);
-    io::stdout().flush();
+        print!("{} + {} = ", left_hand, right_hand);
+        io::stdout().flush();
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read line.");
-    let guess: u32 = guess.trim().parse().expect("expected a number");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line.");
+        let guess: u32 = guess.trim().parse().expect("expected a number");
 
-    match guess.cmp(&answer) {
-        Ordering::Equal => print!("got it"),
-        _ => println!("didn't got it")
+        match guess.cmp(&answer) {
+            Ordering::Equal => print!("got it"),
+            _ => println!("didn't got it")
+        }
     }
+    println!("{} correct, {} mistakes out of {}", correct, mistake, correct + mistake);
 }
