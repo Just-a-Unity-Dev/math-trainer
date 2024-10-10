fe268986c5b23b1837a70e9eb861365a768ceeb0
Rain Caldwell <67359748+Just-a-Unity-Dev@users.noreply.github.com>
2024-10-10 18:03:35 +0800

    duration tracking

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::io;
+use std::time::{Duration,SystemTime};
+use std::cmp::Ordering;
 use std::io::Write;
 use rand::Rng;
-use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     // set stats 
@@ -14,15 +15,23 @@ fn main() {
     let upper_limit = 25;
     let lower_limit = 1;
 
+    let mut v: Vec<Duration> = Vec::new();
+
     loop {
         // new attempt
+        // assign time started
+        let attempt_started = SystemTime::now();
+
         // assign left hand, right hand, and answer
         let left_hand = rng.gen_range(lower_limit..=upper_limit);
         let right_hand = rng.gen_range(lower_limit..=upper_limit);
         let answer = left_hand + right_hand;
 
         print!("{} + {} = ", left_hand, right_hand);
-        io::stdout().flush(); // print equation
+        match io::stdout().flush() {
+            Err(_) => break,
+            Ok(_) => true
+        }; // print equation
 
         // get input
         let mut guess = String::new();
@@ -32,12 +41,24 @@ fn main() {
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => break
-        }
+        };
 
         match guess.cmp(&answer) {
             Ordering::Equal => correct += 1,
             _ => mistake += 1
         }
+        match SystemTime::now().duration_since(attempt_started) {
+            Ok(duration) => v.push(duration),
+            Err(_) => break
+        }
     }
-    println!("{} correct, {} mistakes out of {}", correct, mistake, correct + mistake);
+
+    let mut average_duration: u32 = 0;
+    for attempt_duration in v.iter() {
+        average_duration += attempt_duration.as_millis() as u32;
+    }
+    average_duration = average_duration / v.len() as u32;
+
+    println!("score: {}/{}", correct, correct + mistake);
+    println!("average attempt lasted {} ms", average_duration);
 }
