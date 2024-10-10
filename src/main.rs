379538312948f593d4c13cd72537825b49c68f93
Rain Caldwell <67359748+Just-a-Unity-Dev@users.noreply.github.com>
2024-10-10 17:45:45 +0800

    code cleaup

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,39 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
+    // set stats 
     let mut correct = 0;
     let mut mistake = 0;
 
     let mut rng = rand::thread_rng();
  
+    // assign limits
     let upper_limit = 25;
     let lower_limit = 1;
 
     loop {
+        // new attempt
+        // assign left hand, right hand, and answer
         let left_hand = rng.gen_range(lower_limit..=upper_limit);
         let right_hand = rng.gen_range(lower_limit..=upper_limit);
         let answer = left_hand + right_hand;
 
         print!("{} + {} = ", left_hand, right_hand);
-        io::stdout().flush();
+        io::stdout().flush(); // print equation
 
+        // get input
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
             .expect("failed to read line.");
-        let guess: u32 = guess.trim().parse().expect("expected a number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => break
+        }
 
         match guess.cmp(&answer) {
-            Ordering::Equal => print!("got it"),
-            _ => println!("didn't got it")
+            Ordering::Equal => correct += 1,
+            _ => mistake += 1
         }
     }
     println!("{} correct, {} mistakes out of {}", correct, mistake, correct + mistake);
